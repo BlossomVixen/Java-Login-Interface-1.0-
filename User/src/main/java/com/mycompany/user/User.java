712e25d69e654669b98cb2ce6b8e2f3d09c411ca
Blossom Vixen <matteocarretta@me.com>
2026-04-17 20:08:35 +0200

    Add files via upload

---
+++ User/src/main/java/com/mycompany/user/User.java
@@ -0,0 +1,26 @@
+package com.mycompany.user;
+
+/**
+ *
+ * @author BlossomVixen
+ */
+public class User {
+
+    String name;
+    String email;
+    String password; //cannot be changed nor read after initializing object
+    
+    public User(String name, String email, String password)
+    {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+    
+    public String getName() { return name;} 
+    public String getEMail() { return email;}
+    
+    //avoid making a getPassword() method to later check if the password is correct  (for obvious security reasons)
+    public boolean checkPassword(String str) { return str.equals(password);} 
+
+}
